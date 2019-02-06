bbeb7e723cfa06de1a6d79843d1848630ac60189
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-02-06 11:52:51 +0000

    [minor] Fixed bug in preview script.

--- preview_docs.sh
+++ preview_docs.sh
@@ -13,6 +13,6 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build ${THIS_DIR}/../tk-doc-generator -t tk-doc-generator && \
-docker run --mount type=bind,source="${THIS_DIR}/../..",target=/app tk-doc-generator \
-/app/tk-doc-generator/build_docs.sh --url=http://localhost --url-path=${THIS_DIR}/../_build --source=/app/docs --output=/app/_build && \
+docker run --mount type=bind,source="${THIS_DIR}/..",target=/app tk-doc-generator \
+/app/tk-doc-generator/scripts/build_docs.sh --url=http://localhost --url-path=${THIS_DIR}/../_build --source=/app/docs --output=/app/_build && \
 echo "Documentation built in ${THIS_DIR}/../_build/index.html"
