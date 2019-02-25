be857765f5b862193443de140d69beb8ec85067c
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-02-25 23:13:44 +0000

    fix

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -46,9 +46,10 @@ done
 echo "---------------------------------------------------"
 echo "Shotgun Ecosystem Documentation Build Process"
 echo "---------------------------------------------------"
-echo "Source:     ${SOURCE}"
-echo "Output:     ${OUTPUT}"
-echo "Target Url: ${URL}${URLPATH}"
+echo "Source:     '${SOURCE}'"
+echo "Output:     '${OUTPUT}'"
+echo "Target Url: '${URL}'"
+echo "Url Path:   '${URLPATH}'"
 echo "---------------------------------------------------"
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -80,16 +81,22 @@ echo "Running Jekyll to generate html from markdown..."
 # see if an external override config file exists
 OVERRIDE_CONFIG=${THIS_DIR}/../../jekyll_config.yml
 
+# Write a temp file with the url location to populate
+# base.url based on ${URL}. This is needed for the theme
+# to be populated correctly.
+URL_CONFIG=${TMP_BUILD_FOLDER}/_url_config.yml
+echo "url: '${URL}'" | cat > ${URL_CONFIG}
+
 if [ -e "$OVERRIDE_CONFIG" ]; then
     echo "using override config from ${OVERRIDE_CONFIG}..."
     BUNDLE_GEMFILE=${THIS_DIR}/../Gemfile JEKYLL_ENV=production \
     bundle exec jekyll build \
-    --baseurl "${URLPATH}" --config ${THIS_DIR}/../jekyll/_config.yml,${OVERRIDE_CONFIG} \
+    --config ${THIS_DIR}/../jekyll/_config.yml,${OVERRIDE_CONFIG},${URL_CONFIG} \
     --source "${TMP_BUILD_FOLDER}" --destination "${OUTPUT}"
 else
     BUNDLE_GEMFILE=${THIS_DIR}/../Gemfile JEKYLL_ENV=production \
     bundle exec jekyll build \
-    --baseurl "${URLPATH}" --config ${THIS_DIR}/../jekyll/_config.yml \
+    --config ${THIS_DIR}/../jekyll/_config.yml,${URL_CONFIG} \
     --source "${TMP_BUILD_FOLDER}" --destination "${OUTPUT}"
 fi
 
